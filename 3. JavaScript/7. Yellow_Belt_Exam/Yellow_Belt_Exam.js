4839c57cffc2028d66922e206fe7109934742a13
Your Name <you@example.com>
2023-01-15 05:30:58 -0500

    renamed:    3. JavaScript/2. Second_task/1624039965__video-preview.zip -> 3. JavaScript/2. Video_Preview/1624039965__video-preview.zip 	new file:   3. JavaScript/7. Yellow_Belt_Exam/Yellow_Belt_Exam.css 	new file:   3. JavaScript/7. Yellow_Belt_Exam/Yellow_Belt_Exam.html 	new file:   3. JavaScript/7. Yellow_Belt_Exam/Yellow_Belt_Exam.js 	new file:   3. JavaScript/7. Yellow_Belt_Exam/data_center.png 	new file:   3. JavaScript/7. Yellow_Belt_Exam/green_solutions.png 	new file:   3. JavaScript/7. Yellow_Belt_Exam/logo.png 	new file:   3. JavaScript/7. Yellow_Belt_Exam/main_pic.png 	new file:   3. JavaScript/7. Yellow_Belt_Exam/main_pic_2.png 	new file:   3. JavaScript/7. Yellow_Belt_Exam/promise.png 	new file:   3. JavaScript/7. Yellow_Belt_Exam/search_icon.png

---
+++ 3. JavaScript/7. Yellow_Belt_Exam/Yellow_Belt_Exam.js
@@ -0,0 +1,15 @@
+    function CorporateToLogOut(element){
+        element.innerHTML = "Log Out"
+    }
+
+    function CorporateToLogOut2 (element){
+    element.innerHTML = "Log Out"
+}
+
+    function AreYouSure(element) {
+        alert("You´re going to be re-directed to a registration form")
+    }
+
+    function removeLogo(element){
+        element.remove();
+    }
