3df1f0dd8dae48039900761f8f1f7dbd333df40f
Your Name <you@example.com>
2023-01-22 22:51:12 -0500

    Upload for Video preview
    	deleted:    3. JavaScript/2. Video_Preview/1624039965__video-preview.zip
    	new file:   3. JavaScript/2. Video_Preview/assets/user.png
    	new file:   3. JavaScript/2. Video_Preview/assets/video-placeholder.png
    	new file:   3. JavaScript/2. Video_Preview/index.html
    	new file:   3. JavaScript/2. Video_Preview/script.js
    	new file:   3. JavaScript/2. Video_Preview/style.css
    	new file:   3. JavaScript/2. Video_Preview/video.mp4
    	new file:   5. Python/firrst.py

---
+++ 3. JavaScript/2. Video_Preview/script.js
@@ -0,0 +1,18 @@
+console.log("page loaded...");
+
+var x = document.getElementById("catvideo");
+var muted =  document.getElementById("catvideo");
+
+function PlayVideo() {
+    x.play()
+    Element.getElementById("catvideo")
+    }
+
+function PauseVideo() {
+    x.pause();
+    Element.getElementById("catvideo")
+    }
+
+function MutedVideo() {
+    muted.muted =true
+}
