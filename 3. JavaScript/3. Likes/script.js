48ad7050d2184fa3e994cd7d16856ff829ba96b9
Your Name <you@example.com>
2023-01-24 16:53:09 -0500

    Created ´´likes´´ buttons 	new file:   3. JavaScript/3. Likes/img1.png 	new file:   3. JavaScript/3. Likes/index.html 	new file:   3. JavaScript/3. Likes/script.js 	new file:   3. JavaScript/3. Likes/style.css

---
+++ 3. JavaScript/3. Likes/script.js
@@ -0,0 +1,28 @@
+var CountLikes = 1
+var CountLikesElement = document.querySelector("#CountLikes")
+function add1() {
+    CountLikes++;
+    CountLikesElement.innerText = CountLikes + " like(s)"
+    console.log(CountLikes)
+}
+
+
+var CountLikes2 = 1
+var CountLikes2Element = document.querySelector("#CountLikes2")
+function add2() {
+    CountLikes2++;
+    CountLikes2Element.innerText = CountLikes2 + " like(s)"
+    console.log(CountLikes2)
+}
+
+
+var CountLikes3 = 1
+var CountLikes3Element = document.querySelector("#CountLikes3")
+function add3() {
+    CountLikes3++;
+    CountLikes3Element.innerText = CountLikes3 + " like(s)"
+    console.log(CountLikes3)
+}
+
+
+
